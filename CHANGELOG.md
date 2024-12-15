4ba7fce068f34872c7160c25f6e77684d98e79b8
Sabarinath P <sabarinathp@Sabarinaths-MacBook-Air.local>
2024-12-15 16:26:22 +0530

    package push

---
+++ CHANGELOG.md
@@ -0,0 +1,71 @@
+## 0.0.18
+* Update packages
+
+## 0.0.17
+* Some bug fixings
+* Added new attribute: 
+    - showTopNavbar
+
+## 0.0.16
+* Some bug fixings
+
+## 0.0.15
+* Some bug fixings and improvements
+
+## 0.0.14
+
+* Added Next Weeks
+* Added new attributes
+    - minDate
+    - maxDate
+    - initialDate
+
+## 0.0.13
+
+* Update Preview Image
+
+## 0.0.12
+
+* Added new attributes
+
+    - borderRadius
+    - showNavigationButtons
+    - monthFormat
+
+## 0.0.10
+
+* new attribute added [WeekStartFrom]
+
+## 0.0.9
+
+* Added custom colors
+
+## 0.0.8
+
+* Add example
+
+## 0.0.6
+
+* Fixings
+
+## 0.0.5
+
+* On week change function added
+* On Date select function added
+
+## 0.0.4
+
+* Color Scheme Fixings
+
+## 0.0.3
+
+* Bug Fixings
+
+## 0.0.2
+
+* Some Improvements
+
+
+## 0.0.1
+
+* Init New Package
---
+++ example/lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+// import 'package:horizontal_week_calendar/horizontal_week_calendar.dart';
+import 'package:intl/intl.dart';
+import 'package:packages_test/horizotal_week_calendar/horizontal_week_calendar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Packages Test',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const HorizontalWeekCalendarPackage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HorizontalWeekCalendarPackage extends StatefulWidget {
+  const HorizontalWeekCalendarPackage({super.key});
+
+  @override
+  State<HorizontalWeekCalendarPackage> createState() =>
+      _HorizontalWeekCalendarPackageState();
+}
+
+class _HorizontalWeekCalendarPackageState
+    extends State<HorizontalWeekCalendarPackage> {
+  var selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: const Text(
+          "Horizontal Week Calendar",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              HorizontalWeekCalendar(
+                minDate: DateTime(2023, 12, 31),
+                maxDate: DateTime(2024, 1, 31),
+                initialDate: DateTime(2024, 1, 15),
+                onDateChange: (date) {
+                  setState(() {
+                    selectedDate = date;
+                  });
+                },
+                showTopNavbar: false,
+                monthFormat: "MMMM yyyy",
+                showNavigationButtons: true,
+                weekStartFrom: WeekStartFrom.Monday,
+                borderRadius: BorderRadius.circular(7),
+                activeBackgroundColor: Colors.deepPurple,
+                activeTextColor: Colors.white,
+                inactiveBackgroundColor: Colors.deepPurple.withOpacity(.3),
+                inactiveTextColor: Colors.white,
+                disabledTextColor: Colors.grey,
+                disabledBackgroundColor: Colors.grey.withOpacity(.3),
+                activeNavigatorColor: Colors.deepPurple,
+                inactiveNavigatorColor: Colors.grey,
+                monthColor: Colors.deepPurple,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Selected Date",
+                      textAlign: TextAlign.center,
+                      style: theme.textTheme.titleMedium!.copyWith(
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                    const SizedBox(height: 3),
+                    Text(
+                      DateFormat('dd MMM yyyy').format(selectedDate),
+                      textAlign: TextAlign.center,
+                      style: theme.textTheme.titleLarge!.copyWith(
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
